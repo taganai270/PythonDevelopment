9469847c49443f4bde3b899f966f5298be299a3d
taganai270 <77666297+taganai270@users.noreply.github.com>
2021-02-07 20:49:29 +0500

    Add files via upload

--- module_3.py
+++ module_3.py
@@ -1,19 +1,8 @@
-number = int(input('Введите порядковый номер месяца, от 1 до 12: '))
-while number < 1 or number > 12:
-    print('Ввод не корректен, попробуйте ещё')
-    number = int(input('Введите порядковый номер месяца, от 1 до 12: '))
+def my_func(a, b, c):
+    list_val = sorted([a,b,c])
+    sum_val = list_val[1] + list_val[2]
+    print(sum_val)
+    return
 
-monhts = {1: 'Январь', 2: 'Февраль', 3: 'Март', 4: 'Апрель', 5: 'Май', 6: 'Июнь',
-          7: 'Июль', 8: 'Август', 9: 'Сентябрь', 10: 'Октябрь', 11: 'Ноябрь', 12: 'Декабрь'}
-
-if 1 < number <= 2 or number == 12:
-    print(f'{number} - это зимний месяц  {monhts.get(number)}')
-elif 3 < number <= 5:
-    print(f'{number} - это весенний месяц  {monhts.get(number)}')
-elif 6 < number <= 8:
-    print(f'{number} - это летний месяц  {monhts.get(number)}')
-else:
-    print(f'{number} - это осенний месяц  {monhts.get(number)}')
-
-
+my_func(2,7,1)
 
--- module_6.py
+++ module_6.py
@@ -1,21 +1,9 @@
-goods = []
-goods_list = []
-analytic_dict = {}
-n = 1
-number_goods = int(input('Введите количество товара: '))
-while n <= number_goods:
-    goods = {'название': input('Введите название: '),'цена': input('Введите цену: '),
-                       'количество': input('Введите количество: '), 'ед.изм.': input('Введите единицы измерения: ')}
+def int_func(*args):
+    text = input("Введите текст через пробел:  ").split()
+    for i in range(len(text)):
+        new_text = text[i].capitalize()
+        print(new_text)
+    return
 
-    goods_list.append((n, goods))
-    n += 1
-print(f'\n{goods_list}')
-analytic_dict = {'название': [], 'цена': [], 'количество': [], 'ед.изм.': []}
-for lst in goods_list:
-    analytic_dict['название'].append(lst[1]['название'])
-    analytic_dict['цена'].append(lst[1]['цена'])
-    analytic_dict['количество'].append(lst[1]['количество'])
-    analytic_dict['ед.изм.'].append(lst[1]['ед.изм.'])
-print(f'\n{analytic_dict}')
-
+int_func()
 
