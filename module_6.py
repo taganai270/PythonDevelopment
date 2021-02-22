b54b3f2fb5a72ddca1617180e4ce57fb55931ece
Vladimir Gass <77666297+taganai270@users.noreply.github.com>
2021-02-22 16:32:17 +0800

    Delete module_6.py

--- module_6.py
+++
@@ -1,9 +0,0 @@
-def int_func(*args):
-    text = input("Введите текст через пробел:  ").split()
-    for i in range(len(text)):
-        new_text = text[i].capitalize()
-        print(new_text)
-    return
-
-int_func()
-
